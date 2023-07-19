24c76058f742fc6ab01c78edae02707895036e1b
Hasitha Perera <hasithaperera2017@.com>
2023-07-20 03:38:37 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class AppComponent {
   title = 'untitled';
+  displayFunction: string = 'No Function';
+  displayNumber: number = 0;
+
+  calc(val: any) {
+    switch (val) {
+      case 1:
+        console.log(val);
+        break;
+      case 2:
+        console.log(val);
+        break;
+      case 3:
+        console.log(val);
+        break;
+      case 4:
+        console.log(val);
+        break;
+      case 5:
+        console.log(val);
+        break;
+      case 6:
+        console.log(val);
+        break;
+      case 7:
+        console.log(val);
+        break;
+      case 8:
+        console.log(val);
+        break;
+      case 9:
+        console.log(val);
+        break;
+      case 0:
+        console.log(val);
+        break;
+      case "00":
+        console.log(val);
+        break;
+      case "+":
+        console.log(val);
+        break;
+      case "-":
+        console.log(val);
+        break;
+      case "/":
+        console.log(val);
+        break;
+      case "*":
+        console.log(val);
+        break;
+      case "DEL":
+        console.log(val);
+        break;
+      case "C":
+        console.log(val);
+        break;
+      case "=":
+        console.log(val);
+        break;
+      case ".":
+        console.log(val);
+        break;
+
+    }
+  }
 }
